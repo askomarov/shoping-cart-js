a4c598e97c63262b5b80c3fe02ef412a3b2c3caf
Artem <artem@inisev.com>
2022-09-01 14:11:08 +0200

    rewrite js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,19 @@
 import { products } from "./data.js";
 import { initSwiperSlider } from "./slider.js";
 import { createCatalogItems } from "./createCatalogItems.js";
+import renderCartItems from "./renderCartItems.js";
+import {
+  getNubmerFromPrice,
+  declOfNum,
+  arraysContainSame,
+  sortByAscendingAmount,
+  sortByDescendingPrice,
+} from "./utils.js";
 
 const btnOpenCart = document.querySelector("#colors-cart");
 const btnCloseCart = document.querySelector(".site-cart__close-cart");
 const siteCart = document.querySelector(".site-cart");
+const cartList = document.querySelector(".cart-list");
 
 function closeCart() {
   siteCart.classList.remove("active");
@@ -22,6 +31,7 @@ function openCartModal(evt) {
     btnCloseCart.addEventListener("click", closeCart, { once: true });
   }
 }
+
 btnOpenCart.addEventListener("click", openCartModal);
 
 const catalogListContainer = document.querySelector(".catalog-list");
@@ -41,31 +51,6 @@ navToggle.addEventListener("click", function () {
   }
 });
 
-const URL = "https://baconipsum.com/api/?type=lucky";
-
-function getTextData(cb) {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", URL);
-  xhr.addEventListener("load", () => {
-    const response = JSON.parse(xhr.responseText);
-    cb(response);
-  });
-
-  xhr.addEventListener("error", () => {
-    console.log("error");
-  });
-
-  xhr.send();
-}
-
-function renderPromoText(response) {
-  const dataText = response;
-  promoText.textContent = dataText[0];
-  return;
-}
-
-// getTextData(renderPromoText);
-
 const btnOpenSortModal = document.querySelector("#sort-btn");
 const sortDropdwonMenu = document.querySelector(".sort-list-values");
 
@@ -79,16 +64,41 @@ btnOpenSortModal.addEventListener("click", () => {
   }
 });
 
-sortDropdwonMenu.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  let filterValue = evt.target.dataset.value;
-  let labalText = evt.target.textContent;
-  console.log(labalText);
-  onSelectChangeSortMetod(filterValue);
-  btnOpenSortModal.querySelector("span").textContent = labalText;
+let selectedSortType = "cheap";
+
+const sortCatalog = (catalog) => {
+  switch (selectedSortType) {
+    case "cheap":
+      sortByDescendingPrice(catalog);
+      break;
+    case "expansive":
+      sortByAscendingAmount(catalog);
+      break;
+    case "popular":
+      break;
+    case "newer":
+      break;
+  }
+  return catalog;
+};
+
+function changeSortType() {
+  let checkedInput = sortDropdwonMenu.querySelector("input:checked");
+  let filterValue = checkedInput.value;
+  let labelText = checkedInput.nextElementSibling.textContent;
+  btnOpenSortModal.querySelector("span").textContent = labelText;
+  selectedSortType = filterValue;
+}
+
+function onSortFromChange() {
+  changeSortType();
+  filteredProducts = sortCatalog(filteredProducts);
+
+  updateCatalog(filteredProducts);
   wrapper.classList.remove("modal-open");
   btnOpenSortModal.setAttribute("aria-expanded", "false");
-});
+  return;
+}
 
 const wrapper = document.querySelector(".wrapper");
 const btnOpenFilters = document.querySelector("#filter-btn");
@@ -118,74 +128,180 @@ if (btnOpenFilters) {
   });
 }
 
-const renderFilteredProducts = () => {
+//  CART
+let cartArray = JSON.parse(localStorage.getItem("CART")) || [];
+
+const addItemToCart = (id) => {
+  if (cartArray.some((item) => item.id === id)) {
+    alert("Product alredy in cart");
+  } else {
+    let prod = products.find((item) => item.id === id);
+    cartArray.push({
+      ...prod,
+      countInCart: 1,
+    });
+    return;
+  }
+};
+
+// ///////////
+const onAddBtnHandler = (evt) => {
+  evt.stopPropagation();
+  if (evt.target.classList.contains("product-card__btn-add-tocart")) {
+    const parent = evt.target.closest("[data-product-id]");
+    const id = Number(parent.dataset.productId);
+
+    addItemToCart(id);
+    updateCart();
+    // deleteTaskFromHtml(parent, confirmed);
+  }
+  return;
+};
+//
+const onPlusBtnHandler = (evt) => {
+  evt.stopPropagation();
+  if (evt.target.classList.contains("cart-item__btn--increase")) {
+    const parent = evt.target.closest("[data-productcart-id]");
+    const id = Number(parent.dataset.productcartId);
+    increaseProcudtInCart(id);
+  }
+  return;
+};
+
+function increaseProcudtInCart(id) {
+  cartArray = cartArray.map((item) => {
+    let countInCart = item.countInCart;
+    if (item.id === id && countInCart < item.instock) {
+      countInCart++;
+    }
+    return {
+      ...item,
+      countInCart,
+    };
+  });
+  return updateCart();
+}
+
+const onMinusBtnHandler = (evt) => {
+  evt.stopPropagation();
+  if (evt.target.classList.contains("cart-item__btn--decrease")) {
+    const parent = evt.target.closest("[data-productcart-id]");
+    const id = Number(parent.dataset.productcartId);
+
+    cartArray = cartArray.map((item) => {
+      let countInCart = item.countInCart;
+      if (item.id === id && countInCart > 1) {
+        countInCart--;
+      }
+      return {
+        ...item,
+        countInCart,
+      };
+    });
+    updateCart();
+  }
+  return;
+};
+
+const onDeleteBtnHandler = (evt) => {
+  evt.stopPropagation();
+  if (evt.target.classList.contains("cart-item__delete")) {
+    const parent = evt.target.closest("[data-productcart-id]");
+    const id = Number(parent.dataset.productcartId);
+
+    removeItemFromCart(id);
+    updateCart();
+  }
+  return;
+};
+// cart update
+const getTotalCartPrice = () => {
+  return cartArray.reduce((acc, product) => {
+    return acc + getNubmerFromPrice(product.price) * product.countInCart;
+  }, 0);
+};
+
+const totalCartEl = document.querySelector(".site-cart__total-numbers");
+const totalCartLenghtEl = document.querySelector(".site-cart__items-count");
+const headerCartLinkCount = document.querySelector(
+  ".header-actions__cart-link"
+);
+
+function renderCartItemCountEl() {
+  const words = ["товар", "товара", "товаров"];
+  return (totalCartLenghtEl.textContent = `${cartArray.length} ${declOfNum(
+    cartArray.length,
+    words
+  )}`);
+}
+
+function renderTotalPrice() {
+  const totalPrice = getTotalCartPrice();
+  totalCartEl.textContent = `${totalPrice} ₽`;
+}
+
+function removeItemFromCart(id) {
+  cartArray.map((item) => {
+    if (item.id === id) {
+      cartArray.splice(cartArray.indexOf(item), 1);
+    }
+  });
+  return cartArray;
+}
+
+function updateCart() {
+  renderCartItems(cartArray, cartList);
+  renderTotalPrice();
+  renderCartItemCountEl();
+
+  // if (cartArray.length > 0) {
+  headerCartLinkCount.textContent = cartArray.length || 0;
+  // }
+  // save to localStorage
+  localStorage.setItem("CART", JSON.stringify(cartArray));
+}
+
+let selectedFilters = ["new"];
+let filteredProducts = getFilteredCatalog();
+
+const onChangeFilterForm = () => {
   const checkedValues = [...filtersForm.querySelectorAll("input")]
     .filter((input) => input.checked)
     .map((input) => input.value);
-  if (checkedValues.length === 0) {
-    createCatalogItems(products, catalogListContainer);
-    return;
+  selectedFilters = checkedValues;
+  filteredProducts = getFilteredCatalog();
+  updateCatalog(filteredProducts);
+};
+
+function getFilteredCatalog() {
+  if (selectedFilters.length === 0) {
+    return products;
   } else {
-    const filteredStores = products.filter(({ type }) => {
-      for (let tag of type) {
-        if (checkedValues.includes(tag)) {
-          return true;
-        }
-        return false;
-      }
+    const filteredProducts = products.filter(({ type }) => {
+      return arraysContainSame(selectedFilters, type);
     });
-    createCatalogItems(filteredStores, catalogListContainer);
-    return;
+    return filteredProducts;
   }
-};
+}
 
-const onChangeFilterForm = () => {
-  filtersForm.addEventListener("change", renderFilteredProducts);
-};
+function updateCatalog(catalog) {
+  catalog = sortCatalog(catalog);
+  createCatalogItems(catalog, catalogListContainer);
+}
 
+//  MAIN
 document.addEventListener("DOMContentLoaded", () => {
   initSwiperSlider();
   createCatalogItems(products, catalogListContainer);
-  renderFilteredProducts();
-  onChangeFilterForm();
+  updateCatalog(getFilteredCatalog());
+  updateCart();
+
+  catalogListContainer.addEventListener("click", onAddBtnHandler);
+  cartList.addEventListener("click", onPlusBtnHandler);
+  cartList.addEventListener("click", onMinusBtnHandler);
+  cartList.addEventListener("click", onDeleteBtnHandler);
+
+  filtersForm.addEventListener("change", onChangeFilterForm);
+
+  sortDropdwonMenu.addEventListener("click", onSortFromChange);
 });
-
-function getNubmerFromPrice(str) {
-  return str.split(" ")[0];
-}
-
-const sortByAscendingAmount = (arr) => {
-  return arr.sort((prev, next) => {
-    return getNubmerFromPrice(next.price) - getNubmerFromPrice(prev.price);
-  });
-};
-
-const sortByDescendingPrice = (arr) => {
-  return arr.sort((prev, next) => {
-    return getNubmerFromPrice(prev.price) - getNubmerFromPrice(next.price);
-  });
-};
-
-// console.log(sortByDescendingPrice(products));
-// console.log(sortByAscendingAmount(products));
-
-let sortMetod = "cheap";
-
-function onSelectChangeSortMetod(value) {
-  sortMetod = value;
-  console.log(sortMetod);
-  return;
-}
-
-const sortForm = () => {
-  switch (sortMetod) {
-    case "cheap":
-      break;
-    case "expansive":
-      break;
-    case "popular":
-      break;
-    case "newer":
-      break;
-  }
-};
--- source/js/createCatalogItems.js
+++ source/js/createCatalogItems.js
@@ -1,38 +1,34 @@
-
-
-const createCatalogItems = (itemsList, catalogListParent) => {
+const createCatalogItems = (products, catalogListParent) => {
   catalogListParent.innerHTML = "";
-  // html шаблон который будем копировать
-  const catalogItemTemplate = document
-    .querySelector("#card-tempalate")
-    .content.querySelector(".product-card");
-  const similiarItemsFragment = document.createDocumentFragment();
 
-  itemsList.forEach((item) => {
-    // копируем шаблон со всем его содержимым true в новую переменную
-    const catalogItemElemet = catalogItemTemplate.cloneNode(true);
-
-    catalogItemElemet.dataset.type = `${item.type}`
-    catalogItemElemet.querySelector(".product-card__title ").textContent = item.title;
-    let mainImg = catalogItemElemet.querySelector(".product-card__img-main");
-    mainImg.src = `img/products/${item.img}.png`;
-    mainImg.srcset = `img/products/${item.img}@2x.png 2x`;
-    mainImg.alt = `${item.title}`;
-
-    let hoverImg = catalogItemElemet.querySelector(
-      ".product-card__img-hover"
-    );
-    hoverImg.src = "img/products/product-img-hover.png";
-    hoverImg.srcset = "img/products/product-img-hover@2x.png 2x";
-    hoverImg.alt = `${item.title}`;
-
-    catalogItemElemet.querySelector(".product-card__price").textContent = item.price;
-
-    // добавляем каждый элемент в созданный DocumentFragment
-    similiarItemsFragment.appendChild(catalogItemElemet);
+  products.forEach((product) => {
+    catalogListParent.innerHTML += `
+  <li class="catalog-list__item product-card" data-product-id="${product.id}">
+    <div class="product-card__wrap">
+      <div class="product-card__img-wrap">
+        <img class="product-card__img-main"
+            src="img/products/${product.img}.png"
+            srcset="img/products/${product.img}@2x.png 2x"
+            width="156"
+            height="156"
+            alt="${product.title}">
+        <img class="product-card__img-hover"
+          src="img/products/product-img-hover.png"
+          srcset="img/products/product-img-hover@2x.png 2x"
+          width="156"
+          height="156"
+          alt="${product.title}">
+      </div>
+      <h4 class="product-card__title">${product.title}</h4>
+      <p class="product-card__price">${product.price}</p>
+      <button type="button" class="product-card__btn-add-tocart" aria-label="add product to card">
+        <svg width="16" height="16" aria-hidden="true">
+          <use href="#icon-add"></use>
+        </svg>
+      </button>
+    </div>
+</li>`;
   });
-  // добваляем в родитель фрагмент с готовыми картинками
-  catalogListParent.appendChild(similiarItemsFragment);
 };
 
 export { createCatalogItems };
